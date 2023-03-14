03147c5fab0289fd1fda6ac64e9adeccd4c7c17b
cc <3154205825@qq.com>
2023-03-14 22:19:13 +0800

    项目初始结构创建

---
+++ compon/src/main/java/com/ccmail/compon/ComponApplication.java
@@ -0,0 +1,13 @@
+package com.ccmail.compon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponApplication.class, args);
+    }
+
+}
---
+++ compon/src/test/java/com/ccmail/compon/ComponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccmail.compon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
